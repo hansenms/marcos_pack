f3ee412b8e30fb63bcd03a11e0eb8a9ff565dd0c
Michael Hansen <mihansen@microsoft.com>
2025-05-18 21:08:43 +0000

    Instructions, pre-commit hook, etc.

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Make sure submodules are initialized
+git submodule update --init --recursive
+
+# Install pre-commit hooks
+pre-commit install
+
 # Allocame memory file for the MaRCoS server
 fallocate -l 516KiB /tmp/marcos_server_mem
+
+# Copy the local config for simulation to the client folder
+cp local_config.py marcos_client/
+
